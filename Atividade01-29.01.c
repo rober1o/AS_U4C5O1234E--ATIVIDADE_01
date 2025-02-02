9beb466faaccc2d9eac1bcdb0a95666008cc8058
rober1o <robertoif701@gmail.com>
2025-02-02 17:53:09 -0300

    Criação do callback pra alternar os leds a cada 3 segundos e adição da mensagem a cada 1 segundo

--- Atividade01-29.01.c
+++ Atividade01-29.01.c
@@ -17,15 +17,45 @@ void iniciar_pinos() {
     gpio_set_dir(LED_GREEN, GPIO_OUT);
 }
 
+// Callback do timer, chamado a cada 3 segundos
+bool repeating_time_callback(struct repeating_timer *t) {
+    printf("PASSARAM SE 3 SEGUNDOS\n");
+    if (i == 0){
+        // desliga o led amarelo e verde e liga o vermelho
+        gpio_put(LED_YELLOW, false);
+        gpio_put(LED_GREEN, false);
+        gpio_put(LED_RED, true);
+        i++;
+    }else if(i== 1){
+        // desliga o Led vermelho e verde, e liga o amarelo
+        gpio_put(LED_RED, false);
+        gpio_put(LED_GREEN, false);
+        gpio_put(LED_YELLOW, true);
+        i++;
+    }else if (i == 2){
+        // desliga o led amarelo e vermelho e liga o verde
+        gpio_put(LED_YELLOW, false);
+        gpio_put(LED_RED, false);
+        gpio_put(LED_GREEN, true);
+        i = 0;
+    }
 
+    return true;  // Retorna true para manter o timer rodando
+}
 
 int main() {
-    iniciar_pinos();   
+    iniciar_pinos();   // inicia os pinos
     stdio_init_all();
+ 
+    struct repeating_timer timer;
 
+    // Configura um timer que chama o callback a cada 3000 ms (3 segundos)
+    add_repeating_timer_ms(3000, repeating_time_callback, NULL, &timer);
 
     while (true) {
-
+        // imprime mensagem informado a cada segundo
+        printf("Passou 1 segundo\n");
+        sleep_ms(1000);
     }
 }
 
